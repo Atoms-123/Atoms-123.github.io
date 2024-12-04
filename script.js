79b8e7046a6e363c10af74dfb46d7f677c0cfe49
Atoms-123 <chatuser729@gmail.com>
2024-12-04 23:03:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function navigateTo(page) {
     window.location.href = page;
 }
+
+function showMessage() {
+    const messageDiv = document.getElementById('message');
+    messageDiv.innerText = "Have patience, my dear friend, and wait for it to launch";
+    messageDiv.style.display = "block";
+}
