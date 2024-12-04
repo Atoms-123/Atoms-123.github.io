138f9d3382e8ac199ee8311a5c5465fed096d3ec
Atoms-123 <chatuser729@gmail.com>
2024-12-04 21:43:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 function search() {
     document.getElementById('search-results').innerHTML = `
         <div class="search-options">
-            <button onclick="showProducts('smartwatches')">Smartwatches</button>
-            <button onclick="showProducts('earbuds')">Earbuds</button>
+            <button onclick="showProducts('smartwatches')">
+                Smartwatches
+            </button>
+            <button onclick="showProducts('earbuds')">
+                Earbuds
+            </button>
         </div>
     `;
 }
 
 function showOptions() {
-    const searchOptions = document.getElementById('search-options');
-    searchOptions.style.display = 'block';
-    searchOptions.innerHTML = `
-        <button onclick="showProducts('smartwatches')">Smartwatches</button>
-        <button onclick="showProducts('earbuds')">Earbuds</button>
+    document.getElementById('search-options').style.display = 'block';
+    document.getElementById('search-options').innerHTML = `
+        <button onclick="showProducts('smartwatches')">
+            Smartwatches
+        </button>
+        <button onclick="showProducts('earbuds')">
+            Earbuds
+        </button>
     `;
 }
 
@@ -21,9 +28,16 @@ function showProducts(category) {
 }
 
 function showContact() {
-    const contactInfo = document.getElementById('contact-info');
-    contactInfo.innerHTML = `
-        <p>Please contact us at: <a href="mailto:atomic@example.com">atomicwatches123@gmail.com</a></p>
-    `;
+    let contactInfo = document.getElementById('contact-info');
+    contactInfo.innerHTML = `<p>Please contact us at: 
+        <a href="mailto:atomicwatches123@gmail.com">
+        atomicwatches123@gmail.com
+        </a></p>`;
     contactInfo.style.display = 'block';
+}
+
+function showCart() {
+    let cartInfo = document.getElementById('cart-info');
+    cartInfo.innerHTML = "<p>No item available now.</p>";
+    cartInfo.style.display = 'block';
 }
